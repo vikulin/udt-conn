71e84092ad7ee23c159803a054fd3da36a530be3
vadym <vadym.vikulin@gmail.com>
2022-05-29 12:06:44 +0300

    1. link fix

--- conn.go
+++ conn.go
@@ -12,38 +12,38 @@ type conn struct {
 }
 
 func (c *conn) Read(b []byte) (int, error) {
-	return c.Read(b)
+	return c.udtConn.Read(b)
 }
 
 func (c *conn) Write(b []byte) (int, error) {
-	return c.Write(b)
+	return c.udtConn.Write(b)
 }
 
 // LocalAddr returns the local network address.
 // needed to fulfill the net.Conn interface
 func (c *conn) LocalAddr() net.Addr {
-	return c.LocalAddr()
+	return c.udtConn.LocalAddr()
 }
 
 // RemoteAddr returns the remote network address.
 func (c *conn) RemoteAddr() net.Addr {
-	return c.RemoteAddr()
+	return c.udtConn.RemoteAddr()
 }
 
 func (c *conn) Close() error {
-	return c.Close()
+	return c.udtConn.Close()
 }
 
 func (c *conn) SetDeadline(t time.Time) error {
-	return c.SetDeadline(t)
+	return c.udtConn.SetDeadline(t)
 }
 
 func (c *conn) SetReadDeadline(t time.Time) error {
-	return c.SetReadDeadline(t)
+	return c.udtConn.SetReadDeadline(t)
 }
 
 func (c *conn) SetWriteDeadline(t time.Time) error {
-	return c.SetWriteDeadline(t)
+	return c.udtConn.SetWriteDeadline(t)
 }
 
 var _ net.Conn = &conn{}
