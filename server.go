43bfb52d8673260e0b81aaaa82ae12af31c4e3f1
vadym <vadym.vikulin@gmail.com>
2022-05-29 12:05:06 +0300

    1. link fix

--- server.go
+++ server.go
@@ -14,16 +14,16 @@ var _ net.Listener = &server{}
 
 // Accept waits for and returns the next connection to the listener.
 func (s *server) Accept() (net.Conn, error) {
-	return s.Accept()
+	return s.udtServer.Accept()
 }
 
 // Close closes the listener.
 // Any blocked Accept operations will be unblocked and return errors.
 func (s *server) Close() error {
-	return s.Close()
+	return s.udtServer.Close()
 }
 
 // Addr returns the listener's network address.
 func (s *server) Addr() net.Addr {
-	return s.Addr()
+	return s.udtServer.Addr()
 }
